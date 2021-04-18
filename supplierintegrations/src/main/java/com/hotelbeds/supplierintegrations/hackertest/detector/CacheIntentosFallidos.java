deb8d3a92230c61dcb229c299ada1aedd6f4e56b
gun <gun@Midgard>
2021-04-18 17:09:57 +0200

    Se controla  cuándo debe  limpiar la caché

--- supplierintegrations/src/main/java/com/hotelbeds/supplierintegrations/hackertest/detector/CacheIntentosFallidos.java
+++ supplierintegrations/src/main/java/com/hotelbeds/supplierintegrations/hackertest/detector/CacheIntentosFallidos.java
@@ -6,40 +6,24 @@ import java.util.concurrent.TimeUnit;
 
 class CacheIntentosFallidos {
 
-	private static final long UMBRAL_INTENTO_FALLIDOS = TimeUnit.MINUTES.toMillis(5);
+	private static final long UMBRAL_INTENTO_FALLIDOS_MS = TimeUnit.MINUTES.toMillis(5);
 	private static final int MAXIMO_INTENTOS_FALLIDOS = 5;
 
 	private final Map<String, IntentosFallidos> intentosFallidosPorIp = new HashMap<>();
-	private final LimpiadorEntradasCaducadas limpiadorCache = new LimpiadorEntradasCaducadas();
+	private final LimpiadorEntradasCaducadas limpiadorCache = new LimpiadorEntradasCaducadas(10, TimeUnit.MINUTES);
 
 	public boolean esIpSospechosa(LineaLog linea) {
+		
+		if(limpiadorCache.debeLimpiarCache()) {
+			
+			limpiadorCache.limpiar(intentosFallidosPorIp, linea.getDate(), MAXIMO_INTENTOS_FALLIDOS);
+		}
 
 		IntentosFallidos intentosFallidos = intentosFallidosPorIp.computeIfAbsent(linea.getIp(), (s)-> new  IntentosFallidos());
 
-		int intentosFallidosEnUmbral = intentosFallidos.getIntentosFallidosEnUmbral(linea.getDate(), UMBRAL_INTENTO_FALLIDOS);
+		int intentosFallidosEnUmbral = intentosFallidos.getIntentosFallidosEnUmbral(linea.getDate(), UMBRAL_INTENTO_FALLIDOS_MS);
 		
 		return intentosFallidosEnUmbral >= MAXIMO_INTENTOS_FALLIDOS;
 	}
 	
-//	private void limpiarIntentosFallidosCaducados(long timestampIntentoFallido) {
-//		
-//		long timestampMinimoAnterior = timestampIntentoFallido - UMBRAL_INTENTO_FALLIDOS;
-//		
-//		for (Iterator<Entry<String, IntentosFallidos>> iterator = intentosFallidosPorIp.entrySet().iterator(); iterator.hasNext();) {
-//			
-//			Entry<String, IntentosFallidos> entry = iterator.next();
-//			
-//			if(ultimoIntentoFallidoCaducado(entry, timestampMinimoAnterior)) {
-//				iterator.remove();
-//			}
-//		}
-//	}
-//	
-//	private boolean ultimoIntentoFallidoCaducado(Entry<String, IntentosFallidos> entry, long timestampMinimoAnterior) {
-//		
-//		long ultimoTimestamp = entry.getValue().getUltimoIntentoFallido();
-//		
-//		return ultimoTimestamp < timestampMinimoAnterior;
-//	}
-
 }
--- supplierintegrations/src/main/java/com/hotelbeds/supplierintegrations/hackertest/detector/LimpiadorEntradasCaducadas.java
+++ supplierintegrations/src/main/java/com/hotelbeds/supplierintegrations/hackertest/detector/LimpiadorEntradasCaducadas.java
@@ -3,8 +3,21 @@ package com.hotelbeds.supplierintegrations.hackertest.detector;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.concurrent.TimeUnit;
 
 class LimpiadorEntradasCaducadas {
+	
+	private final long periodoLimpiezaCacheMs;
+	private long timestampUltimaLimpieza;
+	
+	public LimpiadorEntradasCaducadas(long periodoLimpiezaCache, TimeUnit unidadTiempo) {
+		this.periodoLimpiezaCacheMs = unidadTiempo.toMillis(periodoLimpiezaCache);
+		this.timestampUltimaLimpieza = System.currentTimeMillis();
+	}
+	
+	public boolean debeLimpiarCache() {
+		return System.currentTimeMillis() - timestampUltimaLimpieza > periodoLimpiezaCacheMs;
+	}
 
 	public void limpiar(Map<String, IntentosFallidos> cache, long ultimoTimestampFallido, long umbralIntentosFallidos) {
 
@@ -18,6 +31,8 @@ class LimpiadorEntradasCaducadas {
 				iterator.remove();
 			}
 		}
+		
+		timestampUltimaLimpieza = System.currentTimeMillis();
 	}
 
 	private boolean ultimoIntentoFallidoCaducado(Entry<String, IntentosFallidos> entry, long timestampMinimoAnterior) {
